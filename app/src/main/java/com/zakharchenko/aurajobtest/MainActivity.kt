0102748b9b93bd2bf8004f00634b4d31558f6c76
Konstantyn Zakharchenko <kostya@digitalmaker.io>
2023-05-03 15:52:34 +0300

    Prepared Job to run every 15 minutes

--- app/src/main/java/com/zakharchenko/aurajobtest/MainActivity.kt
+++ app/src/main/java/com/zakharchenko/aurajobtest/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.zakharchenko.aurajobtest
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.zakharchenko.aurajobtest.services.NotificationJobService
+import com.zakharchenko.aurajobtest.services.NotificationJobService.Companion.FIFTEEN_MINUTES
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        StartJob()
+    }
+
+    fun StartJob(){
+        val jobId = 1
+        val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val jobInfo = JobInfo.Builder(jobId, ComponentName(this, NotificationJobService::class.java))
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            .setPeriodic(FIFTEEN_MINUTES)
+            .build()
+        val result = jobScheduler.schedule(jobInfo)
+        Log.e("NotificationJobService", "MainActivity: StartJob")
+        if (result == JobScheduler.RESULT_SUCCESS) {
+            Log.d("NotificationJobService", "Job scheduled successfully!")
+        }else{
+            Log.d("NotificationJobService", "Job scheduled NOT successfully!")
+        }
     }
 }
---
+++ app/src/main/java/com/zakharchenko/aurajobtest/services/NotificationJobService.kt
@@ -0,0 +1,19 @@
+package com.zakharchenko.aurajobtest.services
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+/**
+ * Created by Konstantyn Zakharchenko on 03.05.2023.
+ */
+class NotificationJobService : JobService() {
+    companion object {
+        const val FIFTEEN_MINUTES: Long = 15 * 60 * 1000
+    }
+    override fun onStartJob(params: JobParameters): Boolean {
+        return true
+    }
+    override fun onStopJob(params: JobParameters): Boolean {
+        return true
+    }
+}
